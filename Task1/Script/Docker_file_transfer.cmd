3ca38345e479e51f0f3dcb1ee6d4785f2f7eaf64
shrishbc12 <89251544+shrishbc12@users.noreply.github.com>
2026-03-05 16:50:42 +0545

    Update Docker_file_transfer.cmd

--- Task1/Script/Docker_file_transfer.cmd
+++ Task1/Script/Docker_file_transfer.cmd
@@ -1,9 +1,12 @@
+
+# Create docker network
+docker network create mynet
+
 # Run server container
-docker run -it -- name srv -- network mynet python:3.12-slim bash
-echo "Hello this is docker server!">myfile.txt
-python -m http.server 8081
+docker run -d --name srv --network mynet python:3.12-slim sh -c "echo Hello this is docker server! > myfile.txt && python -m http.server 8081"
 
-# Run client container in another terminal and download file
-docker run --rm --name client --network mynet python:3.12-slim bash
-wget http://srv:8081/myfile.txt
+# Wait for server to start
+timeout /t 5
 
+# Run client container and download file
+docker run --rm --name client --network mynet python:3.12-slim sh -c "wget http://srv:8081/myfile.txt"
